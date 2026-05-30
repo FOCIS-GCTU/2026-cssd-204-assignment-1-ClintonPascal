e9013701226e6ca59a5888a4fd4a8ee704c49aaa
Clinton Pascal <2425404203@live.gctu.edu.gh>
2026-05-30 23:29:13 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
 
 def main():
-  pass #code goes here
+  
+    # File: initials.py
+    # Description: Print out my initials.
+    # Assignment Number: 2
+    #
+    # Name: CLINTON PASCAL
+    # STUDENT ID:  2425404203
+    # Email: 2425404203@live.gctu.edu.gh
+    # Grader:
+    #
+    # On my honor, Clinton Pascal, this programming assignment is my own work
+    # and I have not provided this code to any other student.
 
+    print()
+    print("...MCP")
+    print()
 
-
+    print("...MMMM....MMMM........CCCCCCCCCCCC........PPPPPPPPPP.......")
+    print("...MMMM....MMMM........CCCCCCCCCCCC........PPP......PPP.....")
+    print("...MM.MM..MM.MM........CCC.................PPP.......PP.....")
+    print("...MM.MM..MM.MM........CCC.................PPP........P.....")
+    print("...MM.MM..MM.MM........CCC.................PPP.......PP.....")
+    print("...MM..MMMM..MM........CCC.................PPPPPPPPPP.......")
+    print("...MM..MMMM..MM........CCC.................PPP..............")
+    print("...MM...MM...MM........CCC.................PPP..............")
+    print("...MM...MM...MM...**...CCCCCCCCCCCC...**...PPP............**")
+    print("...MM...MM...MM...**...CCCCCCCCCCCC...**...PPP............**")
+    print()
+    
 main()
